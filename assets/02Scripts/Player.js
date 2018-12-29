b2c62e444de48794a7cae28b3680883d03b27b1f
ZhaoYongshuang <17190010703@163.com>
2018-12-29 20:38:06 +0800

    添加图片资源

---
+++ assets/02Scripts/Player.js
@@ -0,0 +1,55 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        moveSpeed: 10,
+        sheetDemage: 1,
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start() {
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, function (event) {
+            switch (event.keyCode) {
+                case cc.macro.KEY.a:
+                    this.leftMove = true;
+                case cc.macro.KEY.d:
+                    this.rightMove = true;
+            }
+        }, this);
+
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, function (event) {
+            switch (event.keyCode) {
+                case cc.macro.KEY.a:
+                    this.leftMove = false;
+                case cc.macro.KEY.d:
+                    this.rightMove = false;
+            }
+        }, this);
+    },
+
+    update (dt) {
+        if(this.leftMove){
+            this.node.x-=dt*this.moveSpeed;
+        }else if(this.rightMove){
+            this.node.x+=dt*this.moveSpeed;
+        }
+    },
+});
