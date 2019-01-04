ee5dc9b95764992f4a49682c3cf2d8ecfa802b2e
ZhaoYongshuang <17190010703@163.com>
2019-01-04 13:33:40 +0800

    添加UIMgr管理，添加敌人多点生成

---
+++ assets/02Scripts/UIMgr.js
@@ -0,0 +1,28 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        //分数Label  
+        labScore:cc.Label,
+        //再来一局button
+        btnAgain:cc.Button, 
+    },
+
+    //设置按钮状态
+    SetButtonState(state){
+        this.btnAgain.node.active=state;
+    },
+    //设置得分
+    SetScore(score){
+        this.labScore.string="Score:"+score.toString();
+    },
+    onLoad () {
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
